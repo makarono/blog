0269a9a38e5bbbd953a416ed0581d0e4234288d7
imroc <roc@imroc.io>
2017-12-16 23:46:16 +0800

    rebuilding site Sat Dec 16 23:46:15 CST 2017

--- deploy.sh
+++ deploy.sh
@@ -15,14 +15,9 @@ git push origin master
 
 # Build the project. 
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
-hugo-algolia
 
 # Go To Public folder
 cd public
-
-# Add algolia search index
-grep -v '"content":' algolia.json>roc.json
-rm -f algolia.json
 
 # Add changes to git.
 git add -A
@@ -40,5 +35,4 @@ git push origin master
 
 # Come Back
 cd ..
-
 
